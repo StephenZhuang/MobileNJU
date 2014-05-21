91b419aa893aba377cb8a2f0924cbcd1e8ebee07
wk121250151 <kswangk@163.com>
2014-05-21 17:42:34 +0800

    添加了我之前写的
    这是一整个项目，你可以先看下。主页我按照你的方法重新写了

---
+++ ZSDX2.0/MenuButton.m
@@ -0,0 +1,31 @@
+//
+//  MenuButton.m
+//  ZSDX2.0
+//
+//  Created by luck-mac on 14-5-21.
+//  Copyright (c) 2014年 zsdx. All rights reserved.
+//
+
+#import "MenuButton.h"
+
+@implementation MenuButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ ZSDX2.0/ZSDX2.0/RegisterCompleteViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterCompleteViewController.h
+//  ZSDX2.0
+//
+//  Created by luck-mac on 14-5-8.
+//  Copyright (c) 2014年 zsdx. All rights reserved.
+//
+
+#import "GetCodeViewController.h"
+
+@interface RegisterCompleteViewController : GetCodeViewController
+
+@end
