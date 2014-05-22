678a8e9e2a00ad342e7fca99a68afbecac604996
wk121250151 <kswangk@163.com>
2014-05-22 11:16:54 +0800

    我把我写的都加到你到项目里了
    原来到东西都没改～只是加了我的那部分

---
+++ MobileNJU/MobileNJU/SelfInfoVC.h
@@ -0,0 +1,13 @@
+//
+//  SelfInfoVC.h
+//  MobileNJU
+//
+//  Created by luck-mac on 14-5-22.
+//  Copyright (c) 2014年 Stephen Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelfInfoVC : UIViewController
+
+@end
