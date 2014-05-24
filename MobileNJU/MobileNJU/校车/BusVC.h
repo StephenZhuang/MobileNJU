5e88847c9f80fe3cf761416135cbd9138a1566d3
wk121250151 <kswangk@163.com>
2014-05-24 19:56:10 +0800

    校车ui写好啦

---
+++ MobileNJU/MobileNJU/校车/BusVC.h
@@ -0,0 +1,13 @@
+//
+//  BusVC.h
+//  MobileNJU
+//
+//  Created by luck-mac on 14-5-24.
+//  Copyright (c) 2014年 Stephen Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+@interface BusVC : BaseViewController
+
+@end
