10c1a07dd58cd0d63c103427bc37b43c3d790ec4
StephenZhuang <379128008@qq.com>
2014-05-29 20:22:20 +0800

    添加 树洞详情

---
+++ MobileNJU/MobileNJU/树洞/TreeHoleDetailViewController.m
@@ -0,0 +1,49 @@
+//
+//  TreeHoleDetailViewController.m
+//  MobileNJU
+//
+//  Created by Stephen Zhuang on 14-5-29.
+//  Copyright (c) 2014年 Stephen Zhuang. All rights reserved.
+//
+
+#import "TreeHoleDetailViewController.h"
+
+@interface TreeHoleDetailViewController ()
+
+@end
+
+@implementation TreeHoleDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
