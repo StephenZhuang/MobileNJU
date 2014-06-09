364c868ee8dddc99cc54f07998a4bc119fac46a8
StephenZhuang <379128008@qq.com>
2014-06-09 10:05:26 +0800

    还原iosapi ， 这里面的东西不用改，后面生成冲突就麻烦了

--- MobileNJU/Frame/iosApi/ApiMNewsList.h
+++ MobileNJU/Frame/iosApi/ApiMNewsList.h
@@ -21,13 +21,13 @@
 	 * @param select  回调函数
 	 * @callback MNewsList_Builder
 	*/
-	-(UpdateOne*)get:(id)delegate selecter:(SEL)select page:(NSInteger) page ;
+	-(UpdateOne*)get:(id)delegate selecter:(SEL)select ;
 	/**
 	 * 新闻列表(分页) /mobile?methodno=MNewsList&debug=1&deviceid=1&userid=fe34a40e-eac6-11e3-b696-ac853d9d52b1&verify=cf09a009-d221-466a-b9f0-d7d3b43dce7c&page=2&limit=10&appid=nju
 	 * @param delegate 回调类
 	 * @param select  回调函数
 	 * @callback MNewsList_Builder
 	*/
-    -(UpdateOne*)load:(id)delegate selecter:(SEL)select page:(NSInteger) page;
+	-(UpdateOne*)load:(id)delegate selecter:(SEL)select ;
 
 @end
