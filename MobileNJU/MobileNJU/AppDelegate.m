3ffcea5e86078ef268187f51d2a4cfef7cfaa29a
wk121250151 <kswangk@163.com>
2014-08-12 16:58:35 +0800

    替换推送AppId

--- MobileNJU/MobileNJU/AppDelegate.m
+++ MobileNJU/MobileNJU/AppDelegate.m
@@ -18,7 +18,7 @@
 #import "JDStatusBarNotification.h"
 #import "RDVTabBarController.h"
 
-#define APP_KEY @"GPojFD7dI99ymfKHmOexnGIZ"
+#define APP_KEY @"AkPTDSp3fqKbqy7Sumedrqws"
 #define REPORT_ID @"d5dd317228"
 
 #define IosAppVersion [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
