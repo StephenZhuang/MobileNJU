ca1b3338c0603054e92f2c9389e683e6901367da
wk121250151 <kswangk@163.com>
2014-08-15 13:30:50 +0800

    江大测试完毕

--- MobileNJU/MobileNJU/跳蚤市场/SecondPage.m
+++ MobileNJU/MobileNJU/跳蚤市场/SecondPage.m
@@ -127,7 +127,7 @@
         label = self.phoneLabel;
     }
     [UIView animateWithDuration:0.2f animations:^{
-        [label setTextColor:[UIColor purpleColor]];
+        [label setTextColor:[UIColor colorWithRed:0 green:147/255.0 blue:242/255.0 alpha:1]];
         [label setFont:[UIFont fontWithName:@"Helvetica" size:10.0]];
         label.transform = CGAffineTransformMakeTranslation(0, -10);
     }];
