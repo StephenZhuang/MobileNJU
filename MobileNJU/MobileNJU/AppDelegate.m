96ed00afc5e78e8e59927149fe36be333fd97f9e
wk121250151 <kswangk@163.com>
2014-10-14 11:52:04 +0800

    掌上河海 修正友盟ID

--- MobileNJU/MobileNJU/AppDelegate.m
+++ MobileNJU/MobileNJU/AppDelegate.m
@@ -84,7 +84,7 @@
 - (void) initUmen
 {
     
-    [MobClick startWithAppkey:@"541526bbfd98c50b120a76d7" reportPolicy:BATCH   channelId:nil];
+    [MobClick startWithAppkey:@"54158d1efd98c50aa110e3fe" reportPolicy:BATCH   channelId:nil];
     NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
     [MobClick setAppVersion:version];
     [ToolUtils setVersion:version];
