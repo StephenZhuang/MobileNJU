cdbc9f780986333da81ec64c0499d9ac35bb8b1f
wk121250151 <kswangk@163.com>
2014-10-15 15:34:26 +0800

    南大修正 键盘返回

--- MobileNJU/MobileNJU/成绩查询/GradeDetailVC.m
+++ MobileNJU/MobileNJU/成绩查询/GradeDetailVC.m
@@ -303,7 +303,7 @@
     [self removeMask];
     [self.schIdTextField resignFirstResponder];
     [self.passwordTextField resignFirstResponder];
-    
+    [self.codeField resignFirstResponder];
 }
 
 
