cc3aedb230541ce71950c9cbe40fba490a3f992e
wk121250151 <kswangk@163.com>
2014-10-15 15:28:07 +0800

    掌上南师大 键盘返回修复

--- MobileNJU/MobileNJU/成绩查询/GradeDetailVC.m
+++ MobileNJU/MobileNJU/成绩查询/GradeDetailVC.m
@@ -299,7 +299,7 @@
     [self removeMask];
     [self.schIdTextField resignFirstResponder];
     [self.passwordTextField resignFirstResponder];
-    
+    [self.codeField resignFirstResponder];
 }
 
 
