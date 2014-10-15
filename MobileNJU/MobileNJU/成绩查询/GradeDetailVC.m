4d53f51dd6fb86382ed6c90e1a89f6d9bc215372
wk121250151 <kswangk@163.com>
2014-10-15 15:28:54 +0800

    掌上南工 键盘返回修正

--- MobileNJU/MobileNJU/成绩查询/GradeDetailVC.m
+++ MobileNJU/MobileNJU/成绩查询/GradeDetailVC.m
@@ -291,7 +291,7 @@
     [self removeMask];
     [self.schIdTextField resignFirstResponder];
     [self.passwordTextField resignFirstResponder];
-    
+    [self.codeField resignFirstResponder];
 }
 
 
