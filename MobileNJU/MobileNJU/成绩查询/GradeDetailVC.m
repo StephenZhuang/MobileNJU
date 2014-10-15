2b12ef94d3a19dca4b2a94b99561c0d3a3d10849
wk121250151 <kswangk@163.com>
2014-10-15 14:56:03 +0800

    掌上药大 修正键盘返回

--- MobileNJU/MobileNJU/成绩查询/GradeDetailVC.m
+++ MobileNJU/MobileNJU/成绩查询/GradeDetailVC.m
@@ -294,7 +294,7 @@
     [self removeMask];
     [self.schIdTextField resignFirstResponder];
     [self.passwordTextField resignFirstResponder];
-    
+    [self.codeField resignFirstResponder];
 }
 
 
